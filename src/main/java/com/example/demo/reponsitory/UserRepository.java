69b40b3f64184ad073cdf920bde38b7d4610055f
Thien <nguyentrannhatthien1710@gmail.com>
2020-10-14 19:56:16 +0700

    done paging search user 2

--- src/main/java/com/example/demo/reponsitory/UserRepository.java
+++ src/main/java/com/example/demo/reponsitory/UserRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User>, PagingAndSortingRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
     Boolean existsByUsername(@NotBlank String username);
 
